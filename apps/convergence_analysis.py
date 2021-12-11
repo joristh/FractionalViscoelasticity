c6e6e0506a34b5a2ef6876197aecf69c6065fadc
Joris <joris.thiel@tum.de>
2021-12-11 14:30:49 +0100

    Convergence analysis for single alpha

--- apps/convergence_analysis.py
+++ apps/convergence_analysis.py
@@ -1,10 +1,18 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from config import *
 
+plt.style.use("bmh")
+font = {'size'   : 12}
+matplotlib.rc('font', **font)
+
+alpha = 1.
+
 dir = config['outputfolder']
-dir += "convergence/alpha1.0/"
+dir_plot = dir + "convergence/plots/"
+dir += f"convergence/alpha{alpha}/"
 
 sol = []
 nsteps  = []
@@ -13,7 +21,9 @@ for filename in os.listdir(dir):
    
    sol.append(np.loadtxt(dir+filename))
    nsteps.append(len(sol[-1]))
-   print(len(sol[-1]))
+   #print(len(sol[-1]))
+
+print("All solutions loaded.")
 
 sol, nsteps = np.array(sol, dtype=object), np.array(nsteps)
 idx = np.argsort(nsteps)
@@ -27,8 +37,27 @@ for i in range(len(nsteps)):
     stride.append(int(nsteps[i]/100))
     plt.plot(t, sol[i][::stride[i]], label=f"dt = {dt[i]}")
 
+plt.xlabel("Time [s]")
+plt.ylabel("Tip displacement [arb. unit]")
+plt.title(f"Alpha= {alpha}")
 plt.legend()
+plt.savefig(dir_plot+f"Solution_{alpha}.pdf", bbox_inches="tight")
 plt.show()
 
+reference = sol[-1][::stride[-1]]
+err = []
+for i in range(len(sol)-1):
+    err.append(np.linalg.norm(sol[i][::stride[i]]-reference, ord=np.inf))
 
-print(dt)
+ord = np.log(err[-2]/err[-1])/np.log(dt[-3]/dt[-2])
+
+plt.plot(dt[:-1], err, "o-")
+plt.title(f"Convergence  -  Alpha= {alpha}  -  Order= {ord:{0}.{3}}")
+plt.yscale("log")
+plt.xscale("log")
+plt.xlabel("$dt$")
+plt.ylabel("$\mathcal{E}_\infty(dt)$")
+plt.savefig(dir_plot+f"Convergence_{alpha}.pdf", bbox_inches="tight")
+plt.show()
+
+print("All plots created.")
